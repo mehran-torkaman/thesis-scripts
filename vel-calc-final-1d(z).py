59644b6b7991f9aab43ae57e239aae1631f1af1e
mehran <torkman.mehran@gmail.com>
2021-05-20 18:33:51 +0430

    adding plot func to velocity-profile in z axis

--- vel-calc-final-1d(z).py
+++ vel-calc-final-1d(z).py
@@ -1,11 +1,13 @@
 from statistics import mean
+import matplotlib.pyplot as plt
 import numpy as np
 #=======================================================================
 #Calculate Velocity Profile
 #=======================================================================
 min_h = 0 #float(input('Enetr Your Minimum Height:'))
 max_h = 44 #float(input('Enter Your Maximum Height:'))
-tickness = 0.25 #float(input('Enter Your Bin Tickness:'))
+tickness = 2 #float(input('Enter Your Bin Tickness:'))
+channel_materials = 'phosphorene'
 z = list()
 for i in np.arange(min_h,max_h,tickness):
     count = i+(tickness/2)
@@ -19,9 +21,9 @@ for j in np.arange(min_h,max_h,tickness):
     obj['chunk'+str(j)+'-'+str(j+tickness)] = []
 for k in np.arange(min_h,max_h,tickness):
     for i in range(len(atom)):
-        if atom[i][6] >= k and atom[i][6] < k+tickness:
-            if atom[i][0] == 2:
-                obj['chunk'+str(k)+'-'+str(k+tickness)].append(atom[i][1])
+        if atom[i][8] >= k and atom[i][8] < k+tickness:
+            if atom[i][2] == 2:
+                obj['chunk'+str(k)+'-'+str(k+tickness)].append(atom[i][3])
 results = list()
 for j in np.arange(min_h,max_h,tickness):
     jam = sum(obj['chunk'+str(j)+'-'+str(j+tickness)])
@@ -35,3 +37,26 @@ for j in np.arange(min_h,max_h,tickness):
 with open('Vx-z.txt', 'w') as fout:
     for item in results:
         fout.write("%s\n" % item)
+#=======================
+# Plot Sides
+#=======================
+x = list()
+y = list()
+
+for item in z:
+    x.append(item)
+
+for item in results:
+    y.append(item)
+ticks = list()
+for i in range(min_h,max_h+2,2):
+    ticks.append(i)
+plt.style.use('fivethirtyeight')
+plt.plot(x,y,color='k',linestyle='-',marker='o',linewidth=2,label=channel_materials)
+plt.title('Velocity Profile for Channel 40A')
+plt.xlabel('Z (A)')
+plt.xticks(ticks)
+plt.ylabel('Vx (A/fs)')
+plt.legend(loc=2)
+plt.tight_layout()
+plt.show()
